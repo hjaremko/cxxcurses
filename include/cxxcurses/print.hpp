d0f5c95caf7cbdf91699af3fbaa7fa928b367d6b
Hubert Jaremko <hjaremko@novomatic-tech.com>
2019-08-30 11:40:31 +0200

    Change print interface to avoid ambiguous calls

--- include/cxxcurses/print.hpp
+++ include/cxxcurses/print.hpp
@@ -16,20 +16,10 @@
 namespace cxxcurses
 {
 // TODO: WINDOW*(cxxcurses::window) versions
-inline void print( const int y, const int x, const std::string& format ) noexcept
-{
-    mvwprintw( stdscr, y, x, format.c_str() );
-}
 
 inline void print( const int y, const int x, const glyph_string& format ) noexcept
 {
     format.print( y, x );
-}
-
-inline void print( const int y, const std::string& format ) noexcept
-{
-    const auto x_centered{ ( getmaxx( stdscr ) - format.size() ) / 2 };
-    mvwprintw( stdscr, y, x_centered, format.c_str() );
 }
 
 inline void print( const int y, const glyph_string& format ) noexcept
@@ -38,10 +28,10 @@ inline void print( const int y, const glyph_string& format ) noexcept
     format.print( y, x_centered );
 }
 
-inline void print( const std::string& format ) noexcept
+inline void print( const std::string_view format ) noexcept
 {
     const auto [ y, x ] = get_current_yx();
-    mvwprintw( stdscr, y, x, format.c_str() );
+    mvwprintw( stdscr, y, x, format.data() );
 }
 
 inline void print( const glyph_string& format ) noexcept
@@ -61,11 +51,34 @@ constexpr void print( const int y,
     print( y, x, parse( format_str, arg ), ( std::forward<Args>( args ) )... );
 }
 
-template <typename... Args>
-constexpr void
-print( const int y, const int x, const std::string_view format_str, Args&&... args ) noexcept
+struct printer
 {
-    print( y, x, glyph_string{ format_str }, ( std::forward<Args>( args ) )... );
+    using coords_t = std::pair<int, std::optional<int>>;
+    coords_t coords;
+
+    template <typename... Args>
+    constexpr void operator()( const std::string_view format_str, Args&&... args )
+    {
+        const auto x = [&]() -> int {
+            if ( coords.second )
+                return *coords.second;
+            else
+                return ( getmaxx( stdscr ) - format_str.size() ) / 2;
+        }();
+
+        print( coords.first, x, glyph_string{ format_str }, ( std::forward<Args>( args ) )... );
+    }
+};
+
+constexpr printer print( const int y, const int x )
+{
+    return { std::make_pair( y, x ) };
+}
+
+// centered
+constexpr printer print( const int y )
+{
+    return { std::make_pair( y, std::nullopt ) };
 }
 
 // print at current cursor position versions
@@ -81,20 +94,6 @@ constexpr void print( const std::string_view format_str, Args&&... args ) noexce
     print( glyph_string{ format_str }, ( std::forward<Args>( args ) )... );
 }
 
-// TODO: centered version
-// centered versions
-template <typename T, typename... Args>
-constexpr void
-print( const int y, const glyph_string& format_str, const T& arg, Args&&... args ) noexcept
-{
-    print( y, parse( format_str, arg ), ( std::forward<Args>( args ) )... );
-}
-
-template <typename... Args>
-constexpr void print( const int y, const std::string_view format_str, Args&&... args ) noexcept
-{
-    print( y, glyph_string{ format_str }, ( std::forward<Args>( args ) )... );
-}
 } // namespace cxxcurses
 
 #endif // CXXCURSES_PRINT_HPP
